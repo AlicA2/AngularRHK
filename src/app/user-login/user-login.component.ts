959a2027e1fadb2567d5ea744f3dc34893371d21
Amar Alić <amar.alic@edu.fit.ba>
2023-06-13 14:10:54 +0200

    Login and Registration Connected to C#

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-user-login',
@@ -8,15 +10,15 @@ import {Component, OnInit} from '@angular/core';
 export class UserLoginComponent implements OnInit{
   signupUsers:any[]=[];
   signupObj:any ={
-    userName:'',
+    name:'',
     email:'',
     password:''
   };
   loginObj:any ={
-    userName:'',
+    name:'',
     password:''
   };
-  constructor() {}
+  constructor(private fb: FormBuilder, private auth: AuthService) {}
   ngOnInit():void {
     const localData = localStorage.getItem('signUpUsers');
     if(localData !=null){
@@ -25,23 +27,36 @@ export class UserLoginComponent implements OnInit{
   }
 
   onSignUp(){
+    console.log(this.signupObj)
+    this.auth.signup(this.signupObj)
+      .subscribe({
+        next:(res)=>{
+          alert(res.message)
+        },
+        error: (err)=>{
+          alert(err?.error.message)
+        }
+      })
     this.signupUsers.push(this.signupObj);
     localStorage.setItem('signUpUsers', JSON.stringify(this.signupUsers));
     this.signupObj ={
-      userName:'',
+      name:'',
       email:'',
       password:''
     };
+
   }
   onLogin(){
-    console;
-    const isUserExist=this.signupUsers.find(m=>m.userName== this.loginObj.userName &&
-    m.password==this.loginObj.password);
-    if(isUserExist!= undefined){
-      alert('User Login Successfully!');
+    console.log(this.loginObj)
+    this.auth.login(this.loginObj)
+      .subscribe({
+        next:(res)=>{
+        alert(res.message)
+    },
+    error: (err)=>{
+          alert(err?.error.message)
     }
-    else {
-      alert('Wrong credentials!');
-    }
+      })
+
   }
 }
