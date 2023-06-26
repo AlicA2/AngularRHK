eab37c6d8ec3bdf2bb524d76d644482018c9858b
Amar Alić <amar.alic@edu.fit.ba>
2023-06-26 18:50:24 +0200

    Contact page connected to backend and Form working

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  @ViewChild('contactForm', { static: true }) contactForm!: NgForm;
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit(form: any): void {
+    const contactData = {
+      Email: form.Email,
+      Name: form.Name,
+      Sadrzaj: form.Sadrzaj
+    };
+
+    this.http.post('https://localhost:7273/api/Contact', contactData)
+      .subscribe(
+        response => {
+          console.log('Contact stored successfully', response);
+          this.contactForm.resetForm();
+        },
+        error => {
+          console.error('Error storing contact', error);
+        }
+      );
+  }
 }
