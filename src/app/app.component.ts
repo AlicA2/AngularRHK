95e80c24ded8d3c92af167f1b1fcc6809111997e
Amar Alić <amar.alic@edu.fit.ba>
2023-09-20 11:51:49 +0200

    Changed Stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import {AutentifikacijaHelper} from "./_helpers/autentifikacija-helper";
+import {LoginInformacije} from "./_helpers/login-informacije";
+import {MojConfig} from "./moj-config";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
+//declare function porukaSuccess(a: string):any;
+//declare function porukaError(a: string):any;
 
 @Component({
   selector: 'app-root',
@@ -7,6 +14,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
-}
+  title = 'angularFitVision';
+  constructor(private httpKlijent:HttpClient, private router:Router) {
+  }
 
+  loginInfo():LoginInformacije {
+    return AutentifikacijaHelper.getLoginInfo();
+  }
+
+  odjava() {
+    let token = MojConfig.http_opcije();
+    AutentifikacijaHelper.setLoginInfo(null);
+
+    this.httpKlijent.post(MojConfig.adresa_servera + "/Autentifikacija/Logout/", null, token)
+      .subscribe((x: any) => {
+        this.router.navigateByUrl("/login");
+        //porukaSuccess("logout uspjesan");
+      });
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {LoginInformacije} from "../_helpers/login-informacije";
+import {AutentifikacijaHelper} from "../_helpers/autentifikacija-helper";
+import {MojConfig} from "../moj-config";
+
+declare function porukaSuccess(a: string):any;
+declare function porukaError(a: string):any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  txtKorisnickiIme:any;
+  txtLozinka:any;
+  korisnik_id:any;
+  constructor(private httpKlijent:HttpClient,private  router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  btnLogiranje() {
+    let saljemo={
+      korisnickoIme:this.txtKorisnickiIme,
+      lozinka:this.txtLozinka
+    }
+    this.httpKlijent.post<LoginInformacije>(MojConfig.adresa_servera+ "/Autentifikacija/Login", saljemo).subscribe(
+      (x:LoginInformacije)=>{
+        if(x.isLogiran){
+          porukaSuccess("login upjesan");
+          AutentifikacijaHelper.setLoginInfo(x);
+          if(x.isPremisijaKorisnik)
+          {
+            this.korisnik_id = x.autentifikacijaToken.korisnickiNalogId;
+          }
+          this.router.navigateByUrl("/pocetna");
+        }
+        else{
+          AutentifikacijaHelper.setLoginInfo(null);
+          porukaError("nesuspjesan login");
+        }
+      }
+    )
+  }
+
+
+
+}
