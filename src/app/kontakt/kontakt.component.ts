25290e91345a1fafdf2f6835352f7b424cf6cbb5
Dženan Magrdžija <dzenan.magrdzija@edu.fit.ba>
2023-09-24 18:52:33 +0200

    kontakt page edited, form works!

--- src/app/kontakt/kontakt.component.ts
+++ src/app/kontakt/kontakt.component.ts
@@ -1,5 +1,13 @@
-// contact-form.component.ts
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { LoginInformacije } from "../_helpers/login-informacije";
+import { AutentifikacijaHelper } from "../_helpers/autentifikacija-helper";
+import { MojConfig } from "../moj-config";
+
+
+declare function porukaSuccess(a: string): any;
+declare function porukaError(a: string): any;
 
 @Component({
   selector: 'app-contact-form',
@@ -9,6 +17,39 @@ import { Component, OnInit } from '@angular/core';
 export class KontaktComponent implements OnInit {
   isVisible = false;
   animationStyles = {};
+
+  kontaktVM = {
+    Ime: '',
+    Prezime: '',
+    Email: '',
+    Telefon: '',
+    Poruka: '',
+    korisnickiNalogID:1
+  };
+
+  constructor(private httpKlijent: HttpClient, private router: Router) { }
+
+  onEmailInput(event: any) {
+    const inputElement = event.target;
+    let inputValue = inputElement.value;
+
+    if (inputValue.length === 1 && !inputValue.includes('@gmail.com')) {
+      inputValue += '@gmail.com';
+
+      this.kontaktVM.Email = inputValue;
+
+      setTimeout(() => {
+        const cursorPosition = inputValue.indexOf('d') + 1;
+        inputElement.setSelectionRange(cursorPosition, cursorPosition);
+      });
+    }
+  }
+
+
+
+
+
+
 
   ngOnInit() {
     // Postavite isVisible i animationStyles prema vašim potrebama
@@ -20,4 +61,42 @@ export class KontaktComponent implements OnInit {
       'animation-name': 'fadeInDown'
     };
   }
+  submitKontaktForm() {
+    if (!this.validateForm()) {
+      return;
+    }
+    this.httpKlijent.post(MojConfig.adresa_servera + "/Kontakt/DodajKontakt", this.kontaktVM).subscribe(
+      (response: any) => {
+        porukaSuccess(response.message);
+        this.resetForm();
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000);
+      },
+      (error) => {
+        porukaError("Greška prilikom slanja kontakta");
+      }
+    );
+  }
+
+  // Function to validate the form
+  validateForm() {
+    // Add your validation logic here
+    if (!this.kontaktVM.Ime || !this.kontaktVM.Prezime || !this.kontaktVM.Email || !this.kontaktVM.Telefon || !this.kontaktVM.Poruka) {
+      porukaError("Molimo popunite sva polja.");
+      return false;
+    }
+    return true;
+  }
+  resetForm() {
+    this.kontaktVM = {
+      Ime: '',
+      Prezime: '',
+      Email: '',
+      Telefon: '',
+      Poruka: '',
+      korisnickiNalogID: null
+    };
+  }
 }
+
