f68114ab8ecaab1dedcb1a9d303029f569eb89ed
Amar Alić <amar.alic@edu.fit.ba>
2023-10-03 21:28:34 +0200

    MeniAdded

---
+++ src/app/meni/meni.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meni',
+  templateUrl: './meni.component.html',
+  styleUrls: ['./meni.component.css']
+})
+export class MeniComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  otvoriFormu:boolean=false;
+  SpasiMeni()
+  {
+
+  }
+}
