6b6ddab0d9d395d0b06bdb656b65f5bf6b9497f9
Dženan Magrdžija <dzenan.magrdzija@edu.fit.ba>
2023-10-11 14:19:53 +0200

    MeniPictureWorks

--- src/app/meni/meni.component.ts
+++ src/app/meni/meni.component.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { MojConfig } from "../moj-config";
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { LoginInformacije } from "../_helpers/login-informacije";
 import { UserAuthService } from "../user-auth.service";
-import {DomSanitizer} from "@angular/platform-browser";
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import {AutentifikacijaHelper} from "../_helpers/autentifikacija-helper";
 
 declare function porukaSuccess(a: string): any;
@@ -15,46 +15,100 @@ declare function porukaError(a: string): any;
   templateUrl: './meni.component.html',
   styleUrls: ['./meni.component.css']
 })
-export class MeniComponent implements OnInit {
-novaForma:boolean=false;
 
-kategorije:any;
-menii:any;
-kategorija={
-  Naziv:''
-};
-meni={
-  Naziv:'',
-  Opis:'',
-  Cijena:0,
-  kategorija_id:1
-};
+export class MeniComponent {
+  loginInformation = AutentifikacijaHelper.getLoginInfo();
 
-  constructor(private httpKlijent: HttpClient,
-              private router: Router,
-              private loginInformacije: LoginInformacije,
-              private userAuthService: UserAuthService,
-              private sant:DomSanitizer) { }
-
-  getJelaByKategorija(kategorijaId: number) {
-    return this.menii.filter((prikaz: any) => prikaz.kategorija_id === kategorijaId);
-  }
-
-  ngOnInit(): void {
-    this.GetKategorije();
-    this.menii=this.GetMeni();
-  }
+  novaForma:boolean=false;
+  otvoriFormuNovu:boolean=false;
 
   otvoriFormu: boolean = false;
-  deleteKategorija(s:any)
-  {
-    this.httpKlijent.delete(MojConfig.adresa_servera + "/Kategorija/DeleteKategorija/" + s)
-      .subscribe(() => {
-        porukaSuccess("Uspjesno izbrisana kategorija");
-        this.GetKategorije();
-      });
+  kategorije:any;
+  menii:any;
+
+  kategorija={
+    Naziv:''
+  };
+  meni: {
+    naziv: string;
+    opis: string;
+    cijena: number;
+    kategorija_id: number | null;
+    slikaBase64: string | null;
+  } = {
+    naziv: '',
+    opis: '',
+    cijena: 0,
+    kategorija_id: null,
+    slikaBase64: null
+  };
+
+  constructor(private httpKlijent: HttpClient,
+              private sanitizer: DomSanitizer,
+              private router: Router,
+              private loginInformacije: LoginInformacije,
+              private userAuthService: UserAuthService) {}
+
+  sanitizeImage(imageData: string): SafeUrl {
+    if (imageData) {
+      // Convert the base64 image data to a safe URL
+      const imageUrl = 'data:image/png;base64,' + imageData;
+      return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+    }
+    return '';
   }
-  otvoriFormuNovu:boolean=false;
+  SpasiKategoriju()
+  {
+    if(!this.ValidirajKategoriju())
+    {
+      return;
+    }
+    this.httpKlijent.post(MojConfig.adresa_servera+"/Kategorija/DodajKategoriju",this.kategorija)
+      .subscribe((x)=>{
+        porukaSuccess("Uspjesno dodana kategorija");
+        this.GetMeni();
+        this.GetKategorije();
+        this.resetirajFormuKategorije();
+      },(y)=>{
+        porukaError("Greska");
+      })
+  }
+
+  dodajMeni() {
+    if (!this.validirajPodatke()) {
+      return;
+    }
+    const meniData = {
+      Naziv: this.meni.naziv,
+      Opis: this.meni.opis,
+      Cijena: this.meni.cijena,
+      kategorija_id: this.meni.kategorija_id,
+      SlikaBase64: this.meni.slikaBase64
+    };
+    console.log(meniData);
+
+    this.httpKlijent.post(MojConfig.adresa_servera+"/Meni/DodajMeni", meniData)
+      .subscribe(
+        (response) => {
+          porukaSuccess("Uspjesno dodan meni");
+          console.log(response);
+          this.meni = {
+            naziv: '',
+            opis: '',
+            cijena: 0,
+            kategorija_id: null,
+            slikaBase64: null
+          };
+          this.GetMeni();
+          this.GetKategorije();
+
+        },
+        (error) => {
+          porukaError("Greška u dodavanju menija");
+          console.error(error);
+        }
+      );
+  }
   ObrisiMeni(s: any) {
     this.httpKlijent.delete(MojConfig.adresa_servera + "/Meni/DeleteMeni/" + s)
       .subscribe(() => {
@@ -63,90 +117,76 @@ meni={
 
       });
   }
-  SpasiMeni() {
-    if(!this.ValidirajMeni())
-    {
-      return;
-    }
-    this.httpKlijent.post(MojConfig.adresa_servera+"/Meni/DodajMeni",this.meni)
-      .subscribe((x)=>{
-        porukaSuccess("Uspjesno dodan meni");
-        this.GetMeni();
-        this.GetKategorije();
-        this.resetirajFormuMenia();
-      },(y)=>{
-        porukaError("Greska");
+
+  GetMeni()
+  {
+    this.httpKlijent.get(MojConfig.adresa_servera+"/Meni/GetAll")
+      .subscribe(x=>{
+        this.menii=x;
+        console.log("Data received from server:", x);
+        console.log("menii array:", this.menii);
       })
   }
-resetirajFormuMenia()
-{
-  this.meni=
-    {
-      Naziv:'',
-      Opis:'',
-      Cijena:0.00,
-      kategorija_id:1
-    }
-}
-  loginInformation = AutentifikacijaHelper.getLoginInfo();
-GetMeni()
-{
-  this.httpKlijent.get(MojConfig.adresa_servera+"/Meni/GetAll")
-    .subscribe(x=>{
-      this.menii=x;
-      console.log("Data received from server:", x);
-      console.log("menii array:", this.menii);
-    })
-}
-SpasiKategoriju()
-{
-  if(!this.ValidirajKategoriju())
+  GetKategorije()
   {
-    return;
+    this.httpKlijent.get(MojConfig.adresa_servera+"/Kategorija/GetAll")
+      .subscribe(x=>{
+        this.kategorije=x;
+        console.log(this.kategorije);
+      })
   }
-  this.httpKlijent.post(MojConfig.adresa_servera+"/Kategorija/DodajKategoriju",this.kategorija)
-    .subscribe((x)=>{
-      porukaSuccess("Uspjesno dodana kategorija");
-      this.GetMeni();
-      this.GetKategorije();
-      this.resetirajFormuKategorije();
-    },(y)=>{
-      porukaError("Greska");
-    })
-}
-resetirajFormuKategorije()
-{
-  this.kategorija=
-    {
-      Naziv:''
-    }
-}
-ValidirajKategoriju()
-{
-  if (!this.kategorija.Naziv) {
-    porukaError("Molimo popunite polje.");
-    return false;
-  }
-  return true;
-}
-  getCategorieName(kategorija_id: number): string {
-    const kategorija = this.kategorije.find((k:any) => k.id === kategorija_id);
-    return kategorija ? kategorija.naziv : 'N/A';
-  }
-  ValidirajMeni()
+  deleteKategorija(s:any)
   {
-    if (!this.meni.Opis||!this.meni.Naziv||!this.meni.Cijena||!this.meni.kategorija_id) {
+    this.httpKlijent.delete(MojConfig.adresa_servera + "/Kategorija/DeleteKategorija/" + s)
+      .subscribe(() => {
+        porukaSuccess("Uspjesno izbrisana kategorija");
+        this.GetKategorije();
+      });
+  }
+  getJelaByKategorija(kategorijaId: number) {
+    return this.menii.filter((prikaz: any) => prikaz.kategorija_id === kategorijaId);
+  }
+  ngOnInit(): void {
+    this.GetKategorije();
+    this.menii=this.GetMeni();
+  }
+  handleSlikaInput(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.meni.slikaBase64 = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  validirajPodatke() {
+    if (!this.meni.naziv || !this.meni.opis || !this.meni.cijena || !this.meni.slikaBase64) {
+      console.log('Niste unijeli sve podatke.');
+      return false;
+    }
+    return true;
+  }
+  resetirajFormuKategorije()
+  {
+    this.kategorija=
+      {
+        Naziv:''
+      }
+  }
+  ValidirajKategoriju()
+  {
+    if (!this.kategorija.Naziv) {
       porukaError("Molimo popunite polje.");
       return false;
     }
     return true;
   }
-GetKategorije()
-{
-  this.httpKlijent.get(MojConfig.adresa_servera+"/Kategorija/GetAll")
-    .subscribe(x=>{
-      this.kategorije=x;
-      console.log(this.kategorije);
-    })
+  getCategorieName(kategorija_id: number): string {
+    const kategorija = this.kategorije.find((k:any) => k.id === kategorija_id);
+    return kategorija ? kategorija.naziv : 'N/A';
+  }
 }
-}
+
+
